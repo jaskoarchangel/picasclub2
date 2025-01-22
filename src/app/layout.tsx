c46fb428cb1441dfdbffc89f5a30fa80aa13157a
heitor jasko <74865102+jaskoarchangel@users.noreply.github.com>
2025-01-22 01:26:05 -0300

    alfa208

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './styles/global.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <link rel="icon" href="/favicon.ico" />
       <title>Picas Club - seu site de rolas e picas duras</title>
       <head/>
       <body>{children}</body>
