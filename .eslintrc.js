c3ea10d45b6334f87139a6857cc129baa7258ee9
heitor jasko <74865102+jaskoarchangel@users.noreply.github.com>
2025-01-22 01:22:17 -0300

    alfa207

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  rules: {
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "semi": "off",
+    "quotes": "off",
+    // Desabilita todas as outras regras
+  },
+};
